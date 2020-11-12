16565625215a308a6074869c89ef05d8118913e7
Gustavo H. M. Silva <gustavohmsilva@tuta.io>
2020-11-12 19:36:28 +0000

    first exercise

--- cmd/countsentence.go
+++ cmd/countsentence.go
@@ -1,6 +1,22 @@
 package cmd
 
-// CountSentence return the size of a given string
-func CountSentence(s string) (int, error) {
-	return len(s), nil
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(countSentenceCmd)
+}
+
+var countSentenceCmd = &cobra.Command{
+	Use:   "countsentence",
+	Short: "countsentence counts a god damm sentence",
+	Long: `countsentence counts a god damm sentence
+	countsentence counts a god damm sentence
+	countsentence counts a god damm sentence`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(len(args[0]))
+	},
 }
